0d937f28e27d6df2daf046882f820adac941800b
HyejinUm <uhj1993@naver.com>
2022-10-10 01:08:32 +0900

    Fix: (#2) front-matter 변경

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,33 +2,29 @@ import Layout from '@src/components/ui/Layout';
 import { getPostBySlug, getAllPostsData } from '@src/utils/getPosts';
 import remarkHtml from '@src/utils/remarkHtml';
 import type { IPost } from '@src/types/post';
+import Post from '@src/components/ui/Post';
 
 interface IParams {
   params: { slug: string };
 }
 
-const Post = ({ postData }: IPost) => {
+const PostDetail = ({ postData }: IPost) => {
   return (
     <Layout>
       <h2>{postData.title}</h2>
       <em>{postData.date}</em>
       <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.description }} />
+      <Post>{postData.contentHtml}</Post>
     </Layout>
   );
 };
 
-export default Post;
+export default PostDetail;
 
 export async function getStaticProps({ params }: IParams) {
-  const post = getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'description',
-  ]);
+  const post = getPostBySlug(params.slug, ['title', 'date', 'slug', 'content']);
 
-  const contentHtml = await remarkHtml(post.description);
+  const contentHtml = await remarkHtml(post.content || '');
 
   return {
     props: {
